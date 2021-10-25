23e7dbba09692512123da053c390019ce5725ca0
Rákóczi, Botond <brakoczi@itron.com>
2021-10-25 18:17:39 +0200

    remove junk

--- LanguageWire.Api/Scripts/main.py
+++ LanguageWire.Api/Scripts/main.py
@@ -1,12 +1,11 @@
 import sys
-from translatorLib import google_translator  
+from translatorLib import google_translator
 
 inputText = sys.argv[1]
 sourceLand = sys.argv[2]
 targetLang = sys.argv[3]
 
-# print("input is", inputText, " target language is ", targetLang)
-
-translator = google_translator()  
-translate_text = translator.translate(inputText,lang_tgt=targetLang,lang_src=sourceLand)  
+translator = google_translator()
+translate_text = translator.translate(
+    inputText, lang_tgt=targetLang, lang_src=sourceLand)
 print(translate_text)
